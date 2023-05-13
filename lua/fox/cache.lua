4be68b370ee046051cffd5c40a29a72704d50ab8
Tobias Fischer <tobb10001@gmail.com>
2023-05-13 21:18:00 +0200

    Add caching module.

---
+++ lua/fox/cache.lua
@@ -0,0 +1,23 @@
+--[[ Caching
+
+Just a simple key value cache, that tracks the age of cache entries. ]]
+
+local M
+
+local cache = {}
+
+function M.set(key, value)
+  cache[key] = { time = os.time(), data = value }
+end
+
+function M.get(key, max_age)
+  if not cache[key] then
+    return nil
+  end
+  if os.time() - cache[key].time > max_age then
+    return nil
+  end
+  return cache.data
+end
+
+return M
